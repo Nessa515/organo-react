929062e8acbc9b2b3f526d1aec1679796b86b2f8
Vanessa Oliveira <vanessaoliveiradasilva515@gmail.com>
2023-08-04 17:33:56 -0400

    initial commit

---
+++ src/components/CampoTexto/index.jsx
@@ -0,0 +1,19 @@
+import './CampoTexto.css'
+
+const CampoTexto = (props) => {
+    const aoDigitado = (evento) => {
+        console.log(evento.target.value);
+    }
+
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {props.label}
+            </label>
+            <input onChange={aoDigitado} required={props.obrigatorio} placeholder={props.placeholder}/>
+        </div>
+    )
+}
+
+export default CampoTexto;
